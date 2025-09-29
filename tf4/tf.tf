54287f8c2ff271069467ee74efb00d47d57ab9bf
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-09-29 15:10:07 -0400

    Check comment summary

--- tf4/tf.tf
+++ tf4/tf.tf
@@ -1,8 +1,8 @@
 resource "null_resource" "foo4" {
 }
 
-resource "null_resource" "bar4" {
+resource "null_resource" "foobar4" {
 }
 
-resource "null_resource" "foobar4" {
+resource "null_resource" "foobar44" {
 }
